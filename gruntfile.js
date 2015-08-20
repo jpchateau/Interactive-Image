0291d3c132970e6cf715094e7e5dc7f74325fa93
Jean-Philippe Chateau <contact@jpchateau.com>
2015-08-20 21:14:53 +0200

    added grunt cssmin

--- gruntfile.js
+++ gruntfile.js
@@ -36,9 +36,21 @@ module.exports = function(grunt) {
           useStrict: true
         }
       }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+        }]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 };
