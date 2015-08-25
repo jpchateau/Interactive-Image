ba399b1f0a7d5c5457b54c29d6d262d9421a05ab
Jean-Philippe Chateau <contact@jpchateau.com>
2015-08-26 00:11:45 +0200

    code climate config

--- gruntfile.js
+++ gruntfile.js
@@ -3,27 +3,8 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       options: {
-        bitwise: true,
-        curly: true,
-        eqnull: true,
-        eqeqeq: true,
-        forin: true,
-        undef: true,
-        unused: true,
-        trailing: true,
-        strict : true,
-        latedef: true,
-        maxparams: 3,
-        nonbsp: true,
-        nonew: true,
-        browser: true,
-        jquery: true,
-        globals: {
-          jQuery: true,
-          define: true,
-          requirejs: true
-        },
-        reporter: require('jshint-stylish')
+        "reporter": require('jshint-stylish'),
+        jshintrc: true
       },
       build: ['scripts/*.js', 'scripts/*/*.js'],
       target: ['file.js']
