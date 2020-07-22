f1c4909f7db17d54604b30ab842bf9b3ccbc2293
jpchateau <contact@jpchateau.com>
2020-07-23 01:22:40 +0200

    updated dev dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const common = require('./webpack.common.js');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
